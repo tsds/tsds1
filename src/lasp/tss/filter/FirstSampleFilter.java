7b830c02b47dff516c056b9e3a07d8cc616eca95
dlindholm <dlindholm@2225bbe2-4109-4f05-a932-a9e4d8c76f3d>
2013-02-25 17:01:05 +0000

    Add filters to retrieve the first or last sample.

---
+++ src/lasp/tss/filter/FirstSampleFilter.java
@@ -0,0 +1,31 @@
+package lasp.tss.filter;
+
+import lasp.tss.TSSPublicException;
+import lasp.tss.variable.IndependentVariable;
+import ucar.ma2.InvalidRangeException;
+import ucar.ma2.Range;
+
+
+public class FirstSampleFilter extends SubsetFilter {
+  //TODO: apply all other exclusions first
+  // FilterConstraint applies SubsetFilter-s immediately
+    
+    protected Range makeRange(IndependentVariable variable) {
+        Range range = variable.getRange();
+        
+        String name = range.getName();
+        int first = range.first();
+        int last = first;
+        int stride = range.stride();
+
+        try {
+            range = new Range(name, first, last, stride);
+        } catch (InvalidRangeException e) {
+            String msg = "Unable to define range for first sample.";
+            throw new TSSPublicException(msg, e);
+        }
+        
+        return range;
+    }
+
+}
